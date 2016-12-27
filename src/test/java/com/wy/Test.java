ad1f863fcccde5c0b1b92d1ae82cb6714580f06c
wangyang15 <QAZWSX@123>
2016-12-27 17:28:49 +0800

    第一个测试用例

--- src/test/java/com/wy/Test.java
+++ src/test/java/com/wy/Test.java
@@ -3,11 +3,18 @@ package com.wy;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import java.math.BigDecimal;
+
 public class Test {
     private static final Logger logger = LoggerFactory.getLogger(Test.class);
 
     @org.testng.annotations.Test
     public void testFirst() {
         logger.info("第一个测试用例");
+        BigDecimal a = new BigDecimal(String.valueOf("123.12"));//成本价
+        BigDecimal b = new BigDecimal(String.valueOf("129.12"));//售卖价
+        BigDecimal c = a.add(b.subtract(a).divide(new BigDecimal("2")));//分销价
+        double d = Math.ceil(c.doubleValue());
+        logger.info(String.valueOf(d));
     }
 }
