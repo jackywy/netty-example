658f8d6873b667c70dbadd2b6dba86e8e9d2e806
wangyang <5201314lp>
2017-01-19 14:47:50 +0800

    经纬度各差0.1度

--- src/main/java/com/wy/Distance.java
+++ src/main/java/com/wy/Distance.java
@@ -10,14 +10,23 @@ public class Distance {
     private static final Logger logger = LoggerFactory.getLogger(Distance.class);
 
     private static final double EARTH_RADIUS = 6378.137;//地球半径
-    private static final Position[] positionArray = {new Position(117.539905, 36.719823), new Position(119.51626, 25.962553)};
 
     private static double rad(double d) {
         return d * Math.PI / 180.0;
     }
 
     public static void main(String args[]) {
-        Distance.getDistance(117.539905, 36.719823, 119.51626, 25.962553);
+        //经度相差0.1度
+        Distance.getDistance(117.4, 36.8, 117.5, 36.8);
+        Distance.getDistance(117.6, 36.8, 117.5, 36.8);
+        //维度相差0.1度
+        Distance.getDistance(117.5, 36.7, 117.5, 36.8);
+        Distance.getDistance(117.5, 36.9, 117.5, 36.8);
+        //经纬度各差0.1度
+        Distance.getDistance(117.4, 36.7, 117.5, 36.8);
+        Distance.getDistance(117.6, 36.7, 117.5, 36.8);
+        Distance.getDistance(117.4, 36.9, 117.5, 36.8);
+        Distance.getDistance(117.6, 36.9, 117.5, 36.8);
     }
 
     public static double getDistance(double lng1, double lat1, double lng2, double lat2) {
